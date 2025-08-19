b51c76c7e44b916e8a21c6283011a7423eafa7cb
sjm-db <steven.mcdonald@databricks.com>
2025-08-19 15:52:51 -0400

    update instructions, sql function fqdn

--- tools/describe_table.sql
+++ tools/describe_table.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION etl_assist_test.tools.describe_table(
+CREATE OR REPLACE FUNCTION describe_table(
   cat STRING COMMENT 'Catalog name',
   sch STRING COMMENT 'Schema name',
   tbl STRING COMMENT 'Table name'
